68e7cbe649035e3b47794e05120472c2cd6de7d3
Jahirul Islam <jahirul@elyranur.com>
2023-01-14 21:22:32 +0600

    ures role added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors = require('cors');
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require("dotenv").config();
 
 const app = express();
 
@@ -10,48 +10,76 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rebcmop.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-  try{
-    const productCategoryCollection = client.db('laptopBazar').collection('categories');
-    const productCollection = client.db('laptopBazar').collection('products');
-    const Booking = client.db('laptopBazar').collection('booking')
+  try {
+    const productCategoryCollection = client
+      .db("laptopBazar")
+      .collection("categories");
+    const productCollection = client.db("laptopBazar").collection("products");
+    const Booking = client.db("laptopBazar").collection("booking");
+    const usersCollection = client.db("laptopBazar").collection("users");
 
-    app.get('/category', async(req, res) => {
+    app.get("/category", async (req, res) => {
       const query = {};
       const option = await productCategoryCollection.find(query).toArray();
       res.send(option);
-    })
+    });
 
-    app.get('/category/:name', async(req, res) => {
+    app.get("/category/:name", async (req, res) => {
       const name = req.params.name;
-      const query = {category: name}
+      const query = { category: name };
       const selectCategory = await productCollection.find(query).toArray();
       res.send(selectCategory);
-    })
+    });
 
-    app.post('/booking', async(req, res) =>{
+    app.post("/booking", async (req, res) => {
       const result = await Booking.insertOne(req.body);
       res.send(result);
-  })
+    });
+    app.get("/booking", async (req, res) => {
+      let query = {};
+      const cursor = Booking.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
 
-  }
-  finally{
+    //Add users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
 
+      const singleUser = await usersCollection.findOne({ email: user?.email });
+
+      if (singleUser?.email) {
+        const message = `${singleUser?.email} already exists`;
+
+        return res.send({ isUserExist: true, message });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    //Get user role by email
+    app.get("/users/role/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ role: user?.role });
+    });
+  } finally {
   }
 }
-run().catch(err => console.log(err))
+run().catch((err) => console.log(err));
 
+app.get("/", async (req, res) => {
+  res.send("laptop bazar server is running");
+});
 
-
-
-app.get('/', async(req, res) => {
-    res.send('laptop bazar server is running');
-})
-
-app.listen(port, () => console.log(`Laptop bazar running on ${port}`))
+app.listen(port, () => console.log(`Laptop bazar running on ${port}`));
